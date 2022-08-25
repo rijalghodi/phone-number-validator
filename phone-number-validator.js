52aa06c3165e389b3008489e111c2832ad9f35c6
rizal ghod <rijalghodi10@gmail.com>
2022-08-25 17:09:31 +0700

    upload phone-number-validator.js

---
+++ phone-number-validator.js
@@ -0,0 +1,44 @@
+/*
+Created by Rijal Ghodi on August 25, 2022
+email : rijalghodi10@gmail.com
+github : https://github.com/rijalghodi
+
+This is a javascript code to solve US phone number validator
+This code will return true if the passed string looks like US phone number
+and return flase if doesn't
+These are the US phone number format looks:
+  555-555-5555
+  (555)555-5555
+  (555) 555-5555
+  555 555 5555
+  5555555555
+If the country code is provided, it has to be 1 and is placed in the first sequence
+*/
+
+function telephoneCheck(str) {
+  const FORMAT_PHONE_NUMBER = [
+    /^(1\s)*\d{3}-\d{3}-\d{4}$/, //phone number format 0
+    /^1*\(\d{3}\)\d{3}\-\d{4}$/, //phone number format 1
+    /^(1\s)*\(\d{3}\)\s\d{3}-\d{4}$/, //phone number format 2
+    /^(1\s)*\d{3}\s\d{3}\s\d{4}$/, // phone number format 3
+    /^(1\s)*\d{10}$/, // phone number format 4
+  ];
+
+  // check whather string passed fits the one of US phone number formats
+  for (let i = 0; i < FORMAT_PHONE_NUMBER.length; i++) {
+    // if it fits one of formats, return true
+    if (FORMAT_PHONE_NUMBER[i].test(str)) {
+      return true;
+    }
+    // if it doesn't fit all of formats, return false
+    else if (i == FORMAT_PHONE_NUMBER.length - 1) {
+      return false;
+    }
+    // if it doesn't fit one format, keep checking
+    else {
+      continue;
+    }
+  }
+}
+
+telephoneCheck("1 (555) 555-5555");
